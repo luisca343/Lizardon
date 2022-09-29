54585b1bff4abbfb976747e9d865f2375e2b993d
Luisca343 <luisca343@gmail.com>
2022-09-29 17:02:03 +0200

    Evento cumplir misión CustomNPC

--- src/main/java/es/allblue/lizardon/event/ModEvents.java
+++ src/main/java/es/allblue/lizardon/event/ModEvents.java
@@ -5,14 +5,19 @@ import es.allblue.lizardon.Lizardon;
 import es.allblue.lizardon.commands.TestCommand;
 import es.allblue.lizardon.init.ItemInit;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import noppes.npcs.api.event.NpcEvent;
+import noppes.npcs.api.event.QuestEvent;
 
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @Mod.EventBusSubscriber(modid = "lizardon")
@@ -23,22 +28,14 @@ public class ModEvents {
     }
 
     @SubscribeEvent
-    public static void onRightClick(PlayerInteractEvent.EntityInteract event) {
-        PlayerEntity player = event.getPlayer();
-        ItemStack stack = event.getItemStack();
-        World world = event.getWorld();
-        if(event.getItemStack().getItem().equals(ItemInit.SMARTROTOM.get())){
-            if(!stack.hasTag()){
-                stack.setTag(new CompoundNBT());
-                stack.getTag().putString("PadURL", "http://www.google.es");
-                int id = Lizardon.PROXY.getNextPadID();
-                stack.getTag().putInt("PadID", id);
-                Lizardon.getLogger().info("Se ha creado el Pad con ID "+id);
-                Lizardon.PROXY.updatePad(id, stack.getTag(), true);
-            }
-            if(event.getTarget() instanceof  PixelmonEntity){
-                Lizardon.getLogger().info("PIXELMON");
-            }
-        }
+    public static void test(NpcEvent.InteractEvent event){
+        Lizardon.getLogger().info("Se ha interactuado con un NPC");
+    }
+
+    @SubscribeEvent
+    public static void misionCumplida(QuestEvent.QuestCompletedEvent event){
+        ServerPlayerEntity jugador = (ServerPlayerEntity) event.player.getMCEntity();
+        jugador.sendMessage(new StringTextComponent("Has completado '" + event.quest.getName()+"'"), UUID.randomUUID());
+        Lizardon.getLogger().info("FINALIZADA");
     }
 }
