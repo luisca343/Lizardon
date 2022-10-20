ddb804e632f6da170333675c8cfd96b429f192d9
Luisca343 <luisca343@gmail.com>
2022-10-20 22:59:51 +0200

    Añadido Funko a Init

--- src/main/java/es/allblue/lizardon/init/BlockInit.java
+++ src/main/java/es/allblue/lizardon/init/BlockInit.java
@@ -1,10 +1,15 @@
 package es.allblue.lizardon.init;
 
 import es.allblue.lizardon.Lizardon;
+import es.allblue.lizardon.blocks.Funko;
 import es.allblue.lizardon.items.LizardonItemGroup;
+import es.allblue.lizardon.items.SmartRotom;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,14 +20,20 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS
             = DeferredRegister.create(ForgeRegistries.BLOCKS, Lizardon.MOD_ID);
 
-    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block){
-        RegistryObject<T> object = BLOCKS.register(name, block);
-        registerBlockItem(name, object);
-        return object;
+    public static final RegistryObject<Block> FUNKO = registerBlock("funko",
+            () -> new Funko(AbstractBlock.Properties.of(Material.STONE)));
 
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
     }
 
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
         ItemInit.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(LizardonItemGroup.LIZARDON_GROUP)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
     }
 }
