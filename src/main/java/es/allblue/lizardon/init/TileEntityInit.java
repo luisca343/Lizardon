df13e1a87fa43dda1ae51e65dd2748019a71d27c
Luisca343 <luisca343@gmail.com>
2022-10-20 22:59:33 +0200

    Inicialización de TileEntity

---
+++ src/main/java/es/allblue/lizardon/init/TileEntityInit.java
@@ -0,0 +1,23 @@
+package es.allblue.lizardon.init;
+
+import es.allblue.lizardon.Lizardon;
+import es.allblue.lizardon.tileentity.FunkoTE;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TileEntityInit {
+    public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES
+            = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, Lizardon.MOD_ID);
+
+
+    public static RegistryObject<TileEntityType<FunkoTE>> FUNKO_TE =
+            TILE_ENTITIES.register("funko_tile", () -> TileEntityType.Builder.of(FunkoTE::new, BlockInit.FUNKO.get()).build(null));
+
+
+    public static void register(IEventBus eventBus){
+        TILE_ENTITIES.register(eventBus);
+    }
+}
