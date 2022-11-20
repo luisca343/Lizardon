2e69b8bc7271dc98fd13ed1031f4c2bfaa3d85c3
Luisca343 <luisca343@gmail.com>
2022-11-20 11:02:48 +0100

    Test entrar region

---
+++ src/main/java/es/allblue/lizardon/net/client/CMessageCambioRegion.java
@@ -0,0 +1,39 @@
+package es.allblue.lizardon.net.client;
+
+
+import com.google.common.base.Charsets;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent;
+import java.util.function.Supplier;
+
+public class CMessageCambioRegion implements Runnable{
+    private String str;
+    private ServerPlayerEntity player;
+
+    public CMessageCambioRegion(String str){
+        this.str = str;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("CAMBIANDO REGION");
+        System.out.println(str);
+
+    }
+
+    public static CMessageCambioRegion decode(PacketBuffer buf) {
+        CMessageCambioRegion message = new CMessageCambioRegion(buf.toString(Charsets.UTF_8));
+        return message;
+    }
+
+    public void encode(PacketBuffer buf) {
+        buf.writeCharSequence(str, Charsets.UTF_8);
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> contextSupplier) {
+        player = contextSupplier.get().getSender();
+        contextSupplier.get().enqueueWork((Runnable) this);
+        contextSupplier.get().setPacketHandled(true);
+    }
+}
