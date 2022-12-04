b3cecdccd959513ee46d89b8bde6ce3cfa39faef
Luisca343 <luisca343@gmail.com>
2022-12-04 22:58:55 +0100

    Update RegionEvents.java

--- src/main/java/es/allblue/lizardon/event/RegionEvents.java
+++ src/main/java/es/allblue/lizardon/event/RegionEvents.java
@@ -1,26 +1,13 @@
 package es.allblue.lizardon.event;
 
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import es.allblue.lizardon.Lizardon;
 import es.allblue.lizardon.net.Messages;
 import es.allblue.lizardon.net.client.CMessageCambioRegion;
-import es.allblue.lizardon.net.client.CMessageDatosServer;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.PacketDistributor;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 import java.util.UUID;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
 
 public class RegionEvents {
 
