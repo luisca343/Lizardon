46e1e4bdd0c4d9ed983a74553fb23506a370e636
Luisca343 <luisca343@gmail.com>
2023-02-11 17:09:48 +0100

    Update LizardonEvents.java

--- src/main/java/es/allblue/lizardon/event/LizardonEvents.java
+++ src/main/java/es/allblue/lizardon/event/LizardonEvents.java
@@ -1,15 +1,17 @@
 package es.allblue.lizardon.event;
 
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.UUID;
 
 
 @Mod.EventBusSubscriber
 public class LizardonEvents {
-    @SubscribeEvent
-    public static void onChat(ClientChatEvent event) {
-        {
-        }
-    }
+
 }
