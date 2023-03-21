951d18a2048070d4777438188c9b44bdcb3d57c7
Luisca343 <luisca343@gmail.com>
2023-03-21 22:57:22 +0100

    :zap: Copiar los vectores de montoyo

---
+++ src/main/java/es/allblue/lizardon/util/vector/Vector2i.java
@@ -0,0 +1,40 @@
+package es.allblue.lizardon.util.vector;
+
+import io.netty.buffer.ByteBuf;
+
+public final class Vector2i {
+
+    public int x;
+    public int y;
+
+    public Vector2i()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public Vector2i(int val)
+    {
+        x = val;
+        y = val;
+    }
+
+    public Vector2i(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2i(ByteBuf bb)
+    {
+        x = bb.readInt();
+        y = bb.readInt();
+    }
+
+    public void writeTo(ByteBuf bb)
+    {
+        bb.writeInt(x);
+        bb.writeInt(y);
+    }
+
+}
