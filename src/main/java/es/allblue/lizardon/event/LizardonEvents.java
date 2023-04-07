3df94d80a56a889b2d463abe7c72ca8050c79ed9
Luisca343 <luisca343@gmail.com>
2023-04-07 23:50:46 +0200

    :sparkles: Registrar comandos

--- src/main/java/es/allblue/lizardon/event/LizardonEvents.java
+++ src/main/java/es/allblue/lizardon/event/LizardonEvents.java
@@ -1,19 +1,33 @@
 package es.allblue.lizardon.event;
 
+import es.allblue.lizardon.commands.Combate;
+import es.allblue.lizardon.commands.Discos;
+import es.allblue.lizardon.commands.TestCommand;
 import es.allblue.lizardon.net.Messages;
 import es.allblue.lizardon.net.client.CMessageVerVideo;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.network.PacketDistributor;
+import net.minecraftforge.server.command.ConfigCommand;
 
 import java.util.UUID;
 
 
 @Mod.EventBusSubscriber
 public class LizardonEvents {
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event){
+        new TestCommand(event.getDispatcher());
+        new Discos(event.getDispatcher());
+        new Combate(event.getDispatcher());
+
+        ConfigCommand.register(event.getDispatcher());
+    }
+
 
     @SubscribeEvent
     public static void onLogin(PlayerEvent.PlayerLoggedInEvent ev){
