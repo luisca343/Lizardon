d35297e5bc861a41d5eeb4dff506f0f01a9551b8
Luisca343 <luisca343@gmail.com>
2023-04-25 19:40:44 +0200

    :sparkles: Acceso a API

--- src/main/java/es/allblue/lizardon/util/WingullAPI.java
+++ src/main/java/es/allblue/lizardon/util/WingullAPI.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WingullAPI {
+    private String LABOON_URL = "http://83.37.185.124:34301/laboon/";
     public static String wingullGET(String str) {
         try {
             URL url = new URL(str);
@@ -43,6 +44,49 @@ public class WingullAPI {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private void getFromLaboon(URL url, String json) throws IOException {
+        WingullAPI.wingullPOST(LABOON_URL + "circuito", json);
+    }
+
+
+    // SSend post request
+    public static void wingullPOST(String str, String json) {
+        new Thread(() -> {
+            try {
+                URL url = new URL(str);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+
+                con.setDoOutput(true);
+                con.addRequestProperty("User-Agent", "Mozilla/4.0");
+                con.setRequestProperty("Content-Type", "application/json");
+
+                OutputStream os = con.getOutputStream();
+                os.write(json.getBytes());
+                os.flush();
+                os.close();
+
+                InputStream inputStream = con.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    response.append(line);
+                }
+                br.close();
+                System.out.println(response.toString());
+                System.out.println("WingullAPI: " + con.getResponseCode());
+
+            } catch (ProtocolException e) {
+                throw new RuntimeException(e);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                System.out.println("WingullAPI: " + e.getMessage());
+            }
+        }).start();
     }
 
 
