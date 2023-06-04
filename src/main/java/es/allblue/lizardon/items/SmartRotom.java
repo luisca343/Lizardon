c0e6837fbb533c5bcbc78253ea8f5dee148b8ca6
Luisca343 <luisca343@gmail.com>
2023-06-04 22:48:41 +0200

    Evento abrir pokédex

--- src/main/java/es/allblue/lizardon/items/SmartRotom.java
+++ src/main/java/es/allblue/lizardon/items/SmartRotom.java
@@ -1,12 +1,19 @@
 package es.allblue.lizardon.items;
 
 
+import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
 import es.allblue.lizardon.Lizardon;
+import es.allblue.lizardon.client.ClientProxy;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.*;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class SmartRotom extends Item {
@@ -14,6 +21,29 @@ public class SmartRotom extends Item {
         super(properties);
     }
 
+    @Override
+    public ActionResultType interactLivingEntity(ItemStack item, PlayerEntity player, LivingEntity entity, Hand hand) {
+        if(entity instanceof PixelmonEntity){
+            System.out.println("Interact with pixelmon");
+            PixelmonEntity pixelmon = (PixelmonEntity) entity;
+
+            int smartRotomID = item.getTag().getInt("PadID");
+            ClientProxy.PadData smartRotom = Lizardon.PROXY.getPadByID(smartRotomID);
+
+            //smartRotom.view.loadURL(Lizardon.config.getHome()+"/smartrotom/dex/datos/"+pixelmon.getSpecies().getDex()+"/base");
+            smartRotom.view.runJS("abrirDex("+ pixelmon.getSpecies().getDex() +")", null);
+
+
+            int num = pixelmon.getSpecies().getDex();
+
+
+            return ActionResultType.SUCCESS;
+        }
+
+        return ActionResultType.FAIL;
+    }
+
+    
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getItemInHand(hand);
