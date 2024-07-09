a926dad720c633af6fd58164cda9d0ca55b29694
Luis Carballo <luisca343@gmail.com>
2024-07-09 11:06:54 +0200

    Update StatChangeActionMixin.java

--- src/main/java/es/boffmedia/teras/mixin/StatChangeActionMixin.java
+++ src/main/java/es/boffmedia/teras/mixin/StatChangeActionMixin.java
@@ -5,6 +5,7 @@ import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "com.pixelmonmod.pixelmon.battles.controller.log.action.type.StatChangeAction")
@@ -12,7 +13,7 @@ public class StatChangeActionMixin {
 
     private PixelmonWrapper pokemon;
 
-    @Inject(at= @At(value = "TAIL"), method = "<init>(ILcom/pixelmonmod/pixelmon/battles/controller/participants/PixelmonWrapper;[I[I)V")
+    @Redirect(at= @At(value = "TAIL"), method = "<init>(ILcom/pixelmonmod/pixelmon/battles/controller/participants/PixelmonWrapper;[I[I)V")
     private void init(int turn, PixelmonWrapper pokemon, int[] oldStats, int[] newStats, CallbackInfo ci) {
         this.pokemon = pokemon;
     }
