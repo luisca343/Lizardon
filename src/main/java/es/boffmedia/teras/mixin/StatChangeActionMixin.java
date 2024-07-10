d76addee1896c03aa8df67a99c5aebe341fae6b7
Luis Carballo <luisca343@gmail.com>
2024-07-10 16:57:50 +0200

    Update StatChangeActionMixin.java

--- src/main/java/es/boffmedia/teras/mixin/StatChangeActionMixin.java
+++ src/main/java/es/boffmedia/teras/mixin/StatChangeActionMixin.java
@@ -1,6 +1,7 @@
 package es.boffmedia.teras.mixin;
 
 
+import com.pixelmonmod.pixelmon.battles.controller.log.action.type.StatChangeAction;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,14 +9,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "com.pixelmonmod.pixelmon.battles.controller.log.action.type.StatChangeAction")
+@Mixin(StatChangeAction.class)
 public class StatChangeActionMixin {
 
     private PixelmonWrapper pokemon;
 
-    @Redirect(at= @At(value = "TAIL"), method = "<init>(ILcom/pixelmonmod/pixelmon/battles/controller/participants/PixelmonWrapper;[I[I)V")
+    @Inject(at = @At(value = "RETURN"), method = "<init>(ILcom/pixelmonmod/pixelmon/battles/controller/participants/PixelmonWrapper;[I[I)V")
     private void init(int turn, PixelmonWrapper pokemon, int[] oldStats, int[] newStats, CallbackInfo ci) {
-        this.pokemon = pokemon;
+        if(pokemon != null) this.pokemon = pokemon;
     }
 
 }
